46b5ad89abef12ca1ff30e9e02ac42df3ea511d9
efomenko <efomenkoone@yandex.ru>
2023-11-23 16:14:27 +0500

    ref: pom changes

--- ewm-stats-service/stats-server/src/main/java/ru/practicum/exception/ExceptionController.java
+++ ewm-stats-service/stats-server/src/main/java/ru/practicum/exception/ExceptionController.java
@@ -1,10 +1,6 @@
 package ru.practicum.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import ru.practicum.entities.ExceptionEntity;
 
 @RestControllerAdvice
 public class ExceptionController {
